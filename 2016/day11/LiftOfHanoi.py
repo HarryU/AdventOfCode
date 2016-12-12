df6da7c9deca87f53adc9227060ad90d4ba7b7e1
Harry Uzzell <harry.uzzell@roke.co.uk>
2016-12-12 17:29:04 +0000

    Progress towards day 11 solution in code.

--- 2016/day11/LiftOfHanoi.py
+++ 2016/day11/LiftOfHanoi.py
@@ -1,35 +1,115 @@
+import copy
+import itertools
+import unittest
+
+
 class LiftOfHanoi:
     def __init__(self):
-        self.currentFloor = 1
-        self.floorContents = {1: ['TG', 'TM', 'PG', 'SG'],
+        self.initialFloor = 1
+        self.initialfloorContents = {1: ['TG', 'TM', 'PG', 'SG'],
                               2: ['PM', 'SM'],
                               3: ['OG', 'OM', 'RG', 'RM'],
                               4: []}
+        self.moves = list()
+        self.states = set()
 
-    def move(self, item1, item2):
-        floorContents = self.floorContents
-        currentFloor = self.currentFloor
-        floorContents[currentFloor].remove(item1)
-        if item2 is not None:
-            floorContents[currentFloor].remove(item2)
-        currentFloor += 1
-        floorContents[currentFloor].append(item1)
-        if item2 is not None:
-            floorContents[currentFloor].append(item2)
-        if self.unpairedGeneratorAndChipPresent:
-            return False
-        self.floorContents = floorContents
-        self.currentFloor = currentFloor
+    def solve(self):
+        goal = sum([len(floorItems) for floorItems in self.initialfloorContents.values()])
+        possibleItems = [items for numberOfItems in range(1, 3) for items in itertools.combinations(self.initialfloorContents[self.initialFloor], numberOfItems)]
+        self.moves = [(move, (copy.deepcopy(self.initialfloorContents), self.initialFloor), 1) for move in self.getPossibleMoves(possibleItems, [1])]
+
+        while len(self.moves) > 0:
+            move, state, iteration = self.moves.pop(0)
+            objects = move[0]
+            direction = move[1]
+            floorContents = state[0]
+            oldFloor = state[1]
+            currentFloor = oldFloor + direction
+            for object in objects:
+                floorContents[oldFloor].remove(object)
+                floorContents[currentFloor].append(object)
+            if len(floorContents[4]) == goal:
+                return iteration
+            if (self.validArrangement(floorContents[currentFloor])) and (self.validArrangement(floorContents[oldFloor])):
+                pairedContents = self.getPairs(floorContents)
+                if frozenset((pairedContents, currentFloor)) not in self.states:
+                    self.states.add(frozenset((pairedContents, currentFloor)))
+                    itemCombinationsAvailableOnThisFloor = [items for numberOfItems in range(1, 3) for items in itertools.combinations(floorContents[currentFloor], numberOfItems)]
+                    minFloor = 1
+                    while len(floorContents[minFloor]) == 0:
+                        minFloor += 1
+                    if currentFloor == 1:
+                        possibleDirections = [1]
+                    elif currentFloor == 4:
+                        possibleDirections = [-1]
+                    else:
+                        possibleDirections = [-1, 1]
+                    newMovesFromCurrentState = [(move, (copy.deepcopy(floorContents), currentFloor), iteration + 1) for move in
+                                   self.getPossibleMoves(itemCombinationsAvailableOnThisFloor, possibleDirections)]
+                    for combo in newMovesFromCurrentState:
+                        self.moves.append(combo)
+        return -1
+
+    def getPairs(self, floorContents):
+        generators = [(floor, item[0]) for floor in range(1, 5) for item in floorContents[floor] if 'G' in item]
+        chips = [(floor, item[0]) for floor in range(1, 5) for item in floorContents[floor] if 'M' in item]
+        pairs = frozenset(frozenset((floorGen, floorChip)) for (floorGen, gen) in generators for (floorChip, chip) in chips if gen == chip)
+        return pairs
+
+    def getPossibleMoves(self, itemCombinationsAvailableOnThisFloor, possibleDirections):
+        return [move for move in itertools.product(itemCombinationsAvailableOnThisFloor, possibleDirections)]
+
+    def validArrangement(self, floor):
+        if len(floor) == 0:
+            return True
+        generators = [item[0] for item in floor if item[1] == 'G']
+        chips = [item[0] for item in floor if item[1] == 'M']
+        if len(generators) == 0:
+            return True
+        if len(chips) == 0:
+            return True
+        for chip in chips:
+            if (chip not in generators) and (len(generators) > 0):
+                return False
         return True
 
-    def unpairedGeneratorAndChipPresent(self):
-        for item in self.floorContents[self.currentFloor]:
-            for otherItem in self.floorContents[self.currentFloor]:
-                if item == otherItem:
-                    continue
-                if item[0] == item[1]:
-                    break
-                return False
 
-    def chooseMove(self):
-        pass
+class TestListOfHanoi(unittest.TestCase):
+    def setUp(self):
+        self.lift = LiftOfHanoi()
+
+    def test_GetFloorsOfPairs(self):
+        pairs = self.lift.getPairs({1: ['HM', 'LM'],
+                                    2: ['HG'],
+                                    3: ['LG'],
+                                    4: []})
+        self.assertEqual(frozenset({frozenset([2, 1]), frozenset([3, 1])}), pairs)
+
+    def test_ValidCheck(self):
+        self.assertTrue(self.lift.validArrangement(['HG', 'HM']))
+        self.assertTrue(self.lift.validArrangement([]))
+        self.assertTrue(self.lift.validArrangement(['LM', 'HM']))
+        self.assertFalse(self.lift.validArrangement(['HG', 'HM', 'LM']))
+        self.assertTrue(self.lift.validArrangement(['HG', 'LG', 'LM']))
+        self.assertTrue(self.lift.validArrangement(['HG', 'HM', 'LM', 'LG']))
+
+    def test_GetPossMoves(self):
+        self.assertEqual([(('HG',), 1), (('HM',), 1), (('HG', 'HM'), 1)], self.lift.getPossibleMoves([('HG',), ('HM',), ('HG', 'HM')], [1]))
+
+    def test_WithExampleInput(self):
+        self.lift.initialfloorContents = {1: ['HM', 'LM'],
+                                          2: ['HG'],
+                                          3: ['LG'],
+                                          4: []}
+        self.assertEqual(11, self.lift.solve())
+
+    def test_WithRealInput(self):
+        self.lift.initialfloorContents = {1: ['SG', 'SM', 'PG', 'PG'],
+                                          2: ['TG', 'RM', 'RG', 'CG', 'CM'],
+                                          3: ['TM'],
+                                          4: []}
+        self.assertEqual(31, self.lift.solve())
+
+if __name__ == '__main__':
+    lift = LiftOfHanoi()
+    print lift.solve()
